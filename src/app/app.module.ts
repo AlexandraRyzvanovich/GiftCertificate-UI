18fd26b694e730b306e3c1d471af834076c8db18
Alexandra Ryzvanovich <alexandraryzvanovich@gmail.com>
2020-09-17 22:14:19 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './login/login.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
